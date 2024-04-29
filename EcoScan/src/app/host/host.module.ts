cebab20fc3ab2fdc551e8fe888870befed14e9eb
e-n-p <nickellam0@gmail.com>
2024-04-29 17:29:33 +0200

    creating initial folder structure

---
+++ EcoScan/src/app/host/host.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HostRoutingModule } from './host-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HostRoutingModule
+  ]
+})
+export class HostModule { }
