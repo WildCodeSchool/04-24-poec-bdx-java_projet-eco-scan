861d5fbe9f7880859163404f18b67fc01ff8d24b
Adam HEMAMOU <h-mamou@hotmail.fr>
2024-05-01 13:24:43 +0200

    start the header & nav-menu

---
+++ EcoScan/src/app/landing-page/landing-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { HeaderNavComponent } from './components/feature/header-nav/header-nav.component';
+import { LandingPagesComponent } from './pages/landing-pages/landing-pages.component';
+
+@NgModule({
+  declarations: [
+    HeaderNavComponent,
+    LandingPagesComponent
+  ],
+  imports: [CommonModule, LandingPageRoutingModule],
+})
+export class LandingPageModule {}
