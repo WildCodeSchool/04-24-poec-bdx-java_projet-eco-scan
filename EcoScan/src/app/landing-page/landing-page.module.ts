f0edd031cebcbd2d8e99206c033ae091ac3aac8d
Adam HEMAMOU <h-mamou@hotmail.fr>
2024-05-03 11:20:32 +0200

    done this.header && this.nav-sidebar need to update profile

--- EcoScan/src/app/landing-page/landing-page.module.ts
+++ EcoScan/src/app/landing-page/landing-page.module.ts
@@ -4,12 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { HeaderNavComponent } from './components/feature/header-nav/header-nav.component';
 import { LandingPagesComponent } from './pages/landing-pages/landing-pages.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    HeaderNavComponent,
-    LandingPagesComponent
-  ],
-  imports: [CommonModule, LandingPageRoutingModule],
+  declarations: [HeaderNavComponent, LandingPagesComponent],
+  imports: [CommonModule, LandingPageRoutingModule, RouterLink, RouterOutlet],
 })
 export class LandingPageModule {}
--- EcoScan/src/app/landing-page/pages/landing-pages/landing-pages.component.ts
+++ EcoScan/src/app/landing-page/pages/landing-pages/landing-pages.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing-pages',
   templateUrl: './landing-pages.component.html',
-  styleUrl: './landing-pages.component.scss'
+  styleUrl: './landing-pages.component.scss',
 })
 export class LandingPagesComponent {
+  isOpen!: boolean;
 
+  onReceivedFromHeader(open: boolean): void {
+    this.isOpen = open;
+  }
 }
