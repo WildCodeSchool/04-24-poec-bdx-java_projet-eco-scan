04e955d40c233b306ee32d5a9cefa5d46b5a1806
Adam HEMAMOU <h-mamou@hotmail.fr>
2024-05-23 12:34:13 +0200

    add waste-card ui

---
+++ EcoScan/src/app/shared-module/components/ui/waste-card/waste-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Rubbish } from '../../../models/types/Rubbish.type';
+
+@Component({
+  selector: 'app-waste-card',
+  templateUrl: './waste-card.component.html',
+  styleUrl: './waste-card.component.scss',
+})
+export class WasteCardComponent {
+  @Input()
+  binType!: Rubbish;
+}
