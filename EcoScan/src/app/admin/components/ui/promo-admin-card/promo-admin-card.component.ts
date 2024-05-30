bddb23ab97c64cdbaec1fed8cb7afb47926f5261
e-n-p <nickellam0@gmail.com>
2024-05-30 11:03:42 +0200

    wip, style and minor page updates for admin pages

--- EcoScan/src/app/admin/components/ui/promo-admin-card/promo-admin-card.component.ts
+++ EcoScan/src/app/admin/components/ui/promo-admin-card/promo-admin-card.component.ts
@@ -1,13 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation, inject } from '@angular/core';
 import { Promo } from '../../../../shared-module/models/types/Promo.type';
+import { DataAccessorService } from '../../../../shared-module/shared/data-accessor.service';
 
 @Component({
   selector: 'app-promo-admin-card',
   templateUrl: './promo-admin-card.component.html',
-  styleUrl: './promo-admin-card.component.scss'
+  styleUrl: './promo-admin-card.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class PromoAdminCardComponent {
 
   @Input()
   promo!:Promo;
+
+  //TODO add method for getting amount of promo's "bought" to
+  private dbAccessor = inject(DataAccessorService);
+
+
+  deletePromo(inPromo: Promo){
+    //TODO when BE created
+    // this.dbAccessor.deletePromo(inPromo).subscribe();
+  }
+
+  suspendPromo(inPromo: Promo){
+    //TODO when BE created
+    // inPromo.isActive = false;
+    // this.dbAccessor.updatePromo(inPromo).subscribe();
+
+  }
 }
