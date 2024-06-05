cf9a10d9b01415dfcc8f05a43a3b2aea159ca610
Adam HEMAMOU <h-mamou@hotmail.fr>
2024-06-05 21:38:43 +0200

    done the right || left animation

--- EcoScan/src/app/staged-waste/staged-waste-routing.module.ts
+++ EcoScan/src/app/staged-waste/staged-waste-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StagedWastePageComponent } from './pages/staged-waste-page/staged-waste-page.component';
 
-const routes: Routes = [{ path: '', component: StagedWastePageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: StagedWastePageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
