67a099c873febbdde9ca887407e9960f0f422037
Victor G <victorgj95@outlook.com>
2024-06-24 15:03:33 +0200

    style in progress

--- EcoScan/src/app/admin/components/ui/menu/menu.component.ts
+++ EcoScan/src/app/admin/components/ui/menu/menu.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HostService } from '../../../../host/shared/host.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
+  private hostService = inject(HostService);
 
+  logout(): void {
+    this.hostService.logout();
+  }
 }
